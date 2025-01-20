33b467c64075e2897bb5cabee5ada526ba1b46a5
Abbas <abbeekhan786@gmail.com>
2025-01-20 11:19:25 +0500

    Star Simulator initialize....

---
+++ src/main/java/org/abbas/controlFlow/StarSimulator.java
@@ -0,0 +1,16 @@
+package org.abbas.controlFlow;
+
+public class StarSimulator {
+    public static void main (String[] args){
+        StarTasks task = new StarTasks();
+        task.showTask1();
+        task.showTask2();
+        task.showTask3();
+        task.showTask4();
+        task.showTask5();
+        task.showTask6();
+        task.showTask7();
+
+
+    }
+}
