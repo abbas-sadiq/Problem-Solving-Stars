c84393e15ff28e0592544ded7586a1281b6c50fb
Abbas <abbeekhan786@gmail.com>
2025-01-20 11:17:14 +0500

    Problem Solving Task in Java

---
+++ src/main/java/org/abbas/controlFlow/StarTasks.java
@@ -0,0 +1,126 @@
+package org.abbas.controlFlow;
+
+public class StarTasks {
+
+    public void showTask1() {
+        System.out.println("\nTask -> 1\n");
+        for (int i = 0; i < 10; i++) {
+            System.out.print(" * ");
+        }
+    }
+
+    public void showTask2() {
+        System.out.println("\nTask -> 2\n");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(" * ");
+        }
+    }
+
+    public void showTask3() {
+        System.out.println("\nTask -> 3\n");
+        for (int i = 0; i < 10; i++) {
+            System.out.print("\n * ");
+            for (int j = 0; j < 10; j++) {
+                System.out.print(" * ");
+            }
+        }
+
+        System.out.println(" ");
+    }
+
+    public void showTask4() {
+        System.out.println("\nTask -> 4\n");
+
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j <= i; j++) {
+
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+
+
+    public void showTask5() {
+        System.out.println("\nTask -> 5\n");
+        int x = 10;
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < x; j++) {
+                if(j < x-i) {
+                    System.out.print("* ");
+                }else {
+                    System.out.print(" ");
+                }
+
+            }
+            System.out.println();
+
+        }
+
+    }
+
+
+    public void showTask6(){
+        System.out.println("\nTask -> 6\n");
+        int x =10;
+
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < x; j++) {
+                if(j < x-i) {
+                    System.out.print("");
+                }else{
+                    System.out.print((" * "));
+                }
+            }
+            System.out.println();
+        }
+
+
+    }
+
+    public void showTask7(){
+        System.out.println("\nTask -> 7\n");
+        int y = 10;
+        for (int i = 0; i <= y; i++) {
+            for (int j = 0; j < y; j++) {
+                if(j < i ) {
+                    System.out.print("  ");
+                }else{
+                    System.out.print("* ");
+                }
+
+            }
+            System.out.println();
+
+        }
+
+    }
+
+    public void showTask8(){
+        System.out.println("\nTask -> 8\n");
+
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j <= i; j++) {
+
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+
+    }
+
+    public void showTask9(){
+        System.out.println("\nTask -> 9\n");
+
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j <= i; j++) {
+
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+    }
+}
